837db9a5d947cf4c3600c223a2c048ae1abe5cd2
berleon <github@leon-sixt.de>
2015-08-19 16:37:02 +0200

    add CaffeGridGenerator
    uses caffe's the new MemoryDataLayer generators to pipe grid images
    into the neuronal network.

---
+++ src/CaffeGridGenerator.h
@@ -0,0 +1,44 @@
+
+#pragma once
+
+#include <memory>
+#include <caffe/solver.hpp>
+#include <caffe/data_layers.hpp>
+#include <boost/smart_ptr.hpp>
+
+#include "deepdecoder.h"
+
+namespace deepdecoder {
+
+class GeneratedGrid;
+
+void generateGridDataset(size_t batch_size, std::vector<cv::Mat> * mats, std::vector<int> * labels);
+
+
+template<typename Dtype>
+class GridGenerator : public caffe::MemoryDataLayer<Dtype>::MatGenerator {
+public:
+    explicit GridGenerator() {}
+    virtual ~GridGenerator() = default;
+    virtual void generate(int batch_size, std::vector<cv::Mat> * mats, std::vector<int> * labels) {
+        return generateGridDataset(batch_size, mats, labels);
+    }
+};
+
+template<typename Dtype>
+inline MemoryDataLayerSPtr<Dtype> extractMemoryDataLayer(boost::shared_ptr<caffe::Net<Dtype>> net) {
+    auto top_layer = net->layers()[0];
+    CHECK(std::string("MemoryData").compare(top_layer->type()) == 0);
+    return boost::dynamic_pointer_cast<caffe::MemoryDataLayer<Dtype>>(top_layer);
+}
+
+template<typename Dtype>
+void addGridGeneratorToMemoryDataLayer(caffe::Solver<Dtype> &solver) {
+    auto grid_gen = boost::make_shared<GridGenerator<Dtype>>();
+    extractMemoryDataLayer<Dtype>(solver.net())->SetMatGenerator(grid_gen);
+    for(auto & net : solver.test_nets()) {
+        extractMemoryDataLayer<Dtype>(net)->SetMatGenerator(grid_gen);
+    }
+}
+
+}
--- src/deepdecoder.h
+++ src/deepdecoder.h
@@ -1,6 +1,14 @@
 
 #pragma once
 
+#include <boost/shared_ptr.hpp>
+#include <caffe/data_layers.hpp>
+
 namespace deepdecoder {
     const size_t TAG_SIZE = 60;
+
+    template<typename Dtype>
+    using MemoryDataLayerSPtr = boost::shared_ptr<caffe::MemoryDataLayer<Dtype>>;
+
+    using dataset_t = std::pair<std::vector<cv::Mat>, std::vector<int>>;
 }
