849867ce9ed962ed36537bca600e89955be3fb53
berleon <github@leon-sixt.de>
2015-12-09 12:24:24 +0100

    [install script] use multiple cores

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,8 +1,18 @@
 #! /usr/bin/env bash
 
+ARCH=$(uname -s)
+if [ "$ARCH" == "Linux" ]; then
+    NPROC="$(nproc)"
+elif [ "$ARCH" == "Darwin" ]; then
+    NPROC="$(sysctl -n hw.ncpu)"
+else
+    echo "$ARCH is not supported"
+    exit 1
+fi
+
 mkdir -p build
 cd build
 cmake ..
-make create_python_pkg
+make -j $NPROC create_python_pkg
 cd python/package
 pip install .
